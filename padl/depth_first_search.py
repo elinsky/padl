cea60f41a930c3c219acad6d41ccf197c3c23906
Brian Elinsky <be2112@gmail.com>
2020-11-10 09:50:36 -0600

    Refactored DepthFirstSearch

--- padl/depth_first_search.py
+++ padl/depth_first_search.py
@@ -4,27 +4,29 @@ from padl.graph import Graph
 
 
 class DepthFirstSearch:
-    """Given an undirected unweighted graph and a source vertex, DepthFirstSearch calculates how many and which vertices
-    in the graph are reachable from the source vertex.
+    """Given an undirected unweighted graph and a source target, DepthFirstSearch calculates how many and which vertices
+    in the graph are reachable from the source target.
 
     Args:
         graph : Graph to search.  Must be of type padl.Graph.
-        source : source vertex from which to begin the depth first search."""
+        source : source target from which to begin the depth first search."""
 
     def __init__(self, graph: Graph, source: typing.Hashable):
-        self.graph = graph
-        self.source = source
         self._count = 0
         self._marked = {}
         for vertex in graph.vertices():
             self._marked[vertex] = False
 
-        self._mark(source)
         self._dfs(source, graph)
 
-    def marked(self, vertex: typing.Hashable) -> bool:
-        """True if there is a path between the source vertex and this vertex.  False if there does not exist a path."""
-        return self._marked[vertex]
+    def marked(self, target: typing.Hashable) -> bool:
+        """True if there is a path between the source vertex and the target vertex.  False if there does not exist a
+        path.
+
+        Args:
+            target : target vertex.
+        """
+        return self._marked[target]
 
     def count(self) -> int:
         """Returns the number of vertices reachable from the source vertex."""
@@ -35,7 +37,8 @@ class DepthFirstSearch:
         self._count += 1
 
     def _dfs(self, vertex: typing.Hashable, graph: Graph) -> None:
-        for adjacent_vertex in graph.adj(vertex):
-            if not self.marked(adjacent_vertex):
-                self._mark(adjacent_vertex)
-                self._dfs(adjacent_vertex, graph)
+        self._mark(vertex)
+        adjacent_vertices = graph.adj(vertex)
+        unmarked_adj_vertices = filter(lambda adj_vertex: not self.marked(adj_vertex), adjacent_vertices)
+        for vertex in unmarked_adj_vertices:
+            self._dfs(vertex, graph)
